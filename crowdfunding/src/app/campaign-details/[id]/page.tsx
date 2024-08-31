1532fc37a4b96be04cfb76febb1ae8474f36db05
t9fiction <sohail.sohailishaq@gmail.com>
2024-08-31 19:11:09 +0500

    donator tag removed

--- crowdfunding/src/app/campaign-details/[id]/page.tsx
+++ crowdfunding/src/app/campaign-details/[id]/page.tsx
@@ -52,6 +52,7 @@ const CampaignDetails: React.FC = () => {
       const fetchDonators = async () => {
         try {
           const data = await getDonations(campaign.pId);
+          console.log("Donators Data", campaign.pId)
           setDonators(data);
         } catch (error) {
           console.error('Failed to fetch donators:', error);
@@ -124,21 +125,6 @@ const CampaignDetails: React.FC = () => {
             </div>
           </div>
 
-          <div>
-            <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">Donators</h4>
-            <div className="mt-[20px] flex flex-col gap-4">
-              {donators.length > 0 ? (
-                donators.map((item, index) => (
-                  <div key={`${item.donator}-${index}`} className="flex justify-between items-center gap-4">
-                    <p className="font-epilogue font-normal text-[16px] text-[#b2b3bd] leading-[26px] break-ll">{index + 1}. {item.donator}</p>
-                    <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] break-ll">{item.donation}</p>
-                  </div>
-                ))
-              ) : (
-                <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">No donators yet. Be the first one!</p>
-              )}
-            </div>
-          </div>
         </div>
 
         <div className="flex-1">
